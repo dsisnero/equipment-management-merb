23bd86996a77d374841bfd0d26db79e08b422999
Dominic Sisneros <dsisnero@gmail.com>
2008-11-17 11:19:29 -0700

    Added fti_circuit bin files from gem

---
+++ bin/fti_circuit_search.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+IF NOT "%~f0" == "~f0" GOTO :WinNT
+@"ruby.exe" "C:/Documents and Settings/dominic e sisneros/My Documents/programming/merb/equipment/bin/fti_circuit_search" %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+:WinNT
+@"ruby.exe" "%~dpn0" %*
