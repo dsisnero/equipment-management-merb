51411aa2e33d8d2365f891157a19096f236cd2b7
Dominic Sisneros <dsisnero@gmail.com>
2008-12-02 11:38:20 -0700

    Added global helper parent_for for views, added site layout

--- app/helpers/global_helpers.rb
+++ app/helpers/global_helpers.rb
@@ -1,5 +1,9 @@
 module Merb
   module GlobalHelpers
+    
+    def parent_layout(layout, &block)
+      render capture(&block), :layout => layout
+    end 
     # helpers defined here available to all views.  
   end
 end
